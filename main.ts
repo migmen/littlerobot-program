f8583c5188fe1b7691fb9cdfaeec020759850626
Miguel Mendez <migmen@users.noreply.github.com>
2022-10-14 11:52:26 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,78 +1,3 @@
-let adjNum = 0
-let servoNum = 0
-let loop = false
-// How to use
-// 
-// 1.Push A to start correction
-// 
-// 2.Push A or B to move each servo
-// 
-// 3.Push A+B to switch to next servo
-// 
-// 4.Loop
-// 
-// 5.Ends when smile is displayed
-// 
-// 6.Reset, then Push B to walk
-// 
-// If Kapakai does not fall over, setting is complete
-function servoAdjust () {
-    adjNum = 0
-    servoNum = 0
-    basic.showNumber(servoNum)
-    loop = true
-    while (loop) {
-        if (input.buttonIsPressed(Button.AB)) {
-            plenbit.savePositon(servoNum, adjNum)
-            servoNum += 1
-            adjNum = 0
-            basic.showNumber(servoNum)
-        } else if (input.buttonIsPressed(Button.A)) {
-            adjNum += 1
-            adjNum = plenbit.servoAdjust(servoNum, adjNum)
-        } else if (input.buttonIsPressed(Button.B)) {
-            adjNum += -1
-            adjNum = plenbit.servoAdjust(servoNum, adjNum)
-        } else if (servoNum > 7) {
-            basic.showIcon(IconNames.Happy)
-            basic.pause(2000)
-            loop = false
-        }
-        if (servoNum == 1) {
-            robotbit.Servo(robotbit.Servos.S1, 89)
-            basic.pause(200)
-            robotbit.Servo(robotbit.Servos.S1, 0)
-            basic.pause(200)
-        } else if (servoNum == 2) {
-            robotbit.Servo(robotbit.Servos.S2, 57)
-            basic.pause(200)
-            robotbit.Servo(robotbit.Servos.S2, 0)
-            basic.pause(200)
-        } else if (servoNum == 3) {
-            robotbit.Servo(robotbit.Servos.S3, 138)
-            basic.pause(200)
-            robotbit.Servo(robotbit.Servos.S3, 0)
-            basic.pause(200)
-        } else if (servoNum == 4) {
-            robotbit.Servo(robotbit.Servos.S4, 61)
-            basic.pause(200)
-            robotbit.Servo(robotbit.Servos.S4, 128)
-            basic.pause(200)
-        } else {
-            basic.showLeds(`
-                . . # . .
-                # . # . #
-                # . # . #
-                . # # # .
-                . . . . .
-                `)
-        }
-    }
-}
-
-
-
-
 function raise_left_hand () {
     if (lefthanded_state == 0) {
         PCA9685.setServoPosition(PCA9685.ServoNum.Servo2, left_hand - 150, 64)
@@ -100,6 +25,13 @@ function right_foot_back () {
 function back () {
     right_foot_back()
     left_foot_back()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
 }
 function right_foot_forward () {
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo5, right_foot + 10, 64)
@@ -112,6 +44,13 @@ function right_foot_forward () {
 function go_ahead () {
     right_foot_forward()
     left_foot_forward()
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 }
 function left_foot_back () {
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo5, right_foot - 10, 64)
@@ -176,6 +115,27 @@ function left_foot_forward () {
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo4, left_leg - 40, 64)
     basic.pause(150)
 }
+function choose_move_action () {
+    basic.showNumber(servoNum)
+    loop = true
+    while (loop) {
+        if (input.buttonIsPressed(Button.AB)) {
+            radio.sendNumber(servoNum)
+        } else if (input.buttonIsPressed(Button.A)) {
+            adjNum += 1
+            adjNum = servoNum
+            basic.showNumber(servoNum)
+        } else if (input.buttonIsPressed(Button.B)) {
+            adjNum += -1
+            adjNum = servoNum
+            basic.showNumber(servoNum)
+        } else if (servoNum > 8) {
+            basic.showIcon(IconNames.Sad)
+            basic.pause(2000)
+            loop = false
+        }
+    }
+}
 function reset () {
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo3, right_leg, 64)
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo5, right_foot, 64)
@@ -183,6 +143,7 @@ function reset () {
     PCA9685.setServoPosition(PCA9685.ServoNum.Servo6, left_foot, 64)
     basic.pause(100)
 }
+let loop = false
 let action = 0
 let left_foot = 0
 let left_leg = 0
@@ -192,8 +153,12 @@ let right_hand = 0
 let left_hand = 0
 let right_hand_state = 0
 let lefthanded_state = 0
+let servoNum = 0
+let adjNum = 0
 basic.showIcon(IconNames.TShirt)
 radio.setGroup(1)
+adjNum = 0
+servoNum = 0
 lefthanded_state = 0
 right_hand_state = 0
 left_hand = 170
@@ -211,6 +176,7 @@ PCA9685.setServoPosition(PCA9685.ServoNum.Servo5, right_foot, 64)
 PCA9685.setServoPosition(PCA9685.ServoNum.Servo6, left_foot, 64)
 basic.pause(2000)
 basic.forever(function () {
+    choose_move_action()
     if (action == 1) {
         go_ahead()
     } else if (action == 2) {
